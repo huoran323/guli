d50c5f4d53a9e2e389fa62067e7b2b0df422c9e9
xiaoran <303176152@qq.com>
2020-06-04 16:31:26 +0800

    添加注册中心

--- guli_teacher/src/main/java/com/guli/teacher/TeacherApplication.java
+++ guli_teacher/src/main/java/com/guli/teacher/TeacherApplication.java
@@ -3,8 +3,10 @@ package com.guli.teacher;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TeacherApplication {
 
     public static void main(String[] args) {
